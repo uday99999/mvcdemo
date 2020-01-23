aef681a0f3e362860085d2a41a2c76d53aac645d
uday99 <udaypeddu9999@gmail.com>
2020-01-22 19:53:41 -0600

    fixed the age of an employee

--- Controllers/StudentController.cs
+++ Controllers/StudentController.cs
@@ -15,7 +15,7 @@ namespace studentIndex.Controllers
             var studentList = new List<Student>{
                             new Student() { StudentId = 1, StudentName = "John", Age = 18 } ,
                             new Student() { StudentId = 2, StudentName = "Steve",  Age = 21 } ,
-                            new Student() { StudentId = 3, StudentName = "Bill",  Age = 25 } ,
+                            new Student() { StudentId = 3, StudentName = "Bill",  Age = 35 } ,
                             new Student() { StudentId = 4, StudentName = "Ram" , Age = 20 } ,
                             new Student() { StudentId = 5, StudentName = "Ron" , Age = 31 } ,
                             new Student() { StudentId = 4, StudentName = "Chris" , Age = 17 } ,

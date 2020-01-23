f61d9e8e725370563138667ac8da600ac306f115
uday99 <udaypeddu9999@gmail.com>
2020-01-22 19:49:20 -0600

    Initial commit

---
+++ Controllers/StudentController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using studentIndex.Models;
+
+namespace studentIndex.Controllers
+{
+    public class StudentController : Controller
+    {
+        // GET: Student
+        public ActionResult Index()
+        {
+            var studentList = new List<Student>{
+                            new Student() { StudentId = 1, StudentName = "John", Age = 18 } ,
+                            new Student() { StudentId = 2, StudentName = "Steve",  Age = 21 } ,
+                            new Student() { StudentId = 3, StudentName = "Bill",  Age = 25 } ,
+                            new Student() { StudentId = 4, StudentName = "Ram" , Age = 20 } ,
+                            new Student() { StudentId = 5, StudentName = "Ron" , Age = 31 } ,
+                            new Student() { StudentId = 4, StudentName = "Chris" , Age = 17 } ,
+                            new Student() { StudentId = 4, StudentName = "Rob" , Age = 19 }
+                        };
+            // Get the students from the database in the real application
+
+            return View(studentList);
+
+        }
+    }
+}
---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="studentIndex.MvcApplication" Language="C#" %>
